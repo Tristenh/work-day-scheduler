e6aac3b34c8c1b49ad6c242e5d95b2139e21fad9
Tristenh <tristenhobba1@gmail.com>
2023-06-17 02:02:45 +1000

    updated ids of rows in HTML, saveBtn event listener, rowId = $(this) the button clicked and closest(row) the row wich is closest to the button and gets the attribute of (id)

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const container = $(".container-fluid px-5")
+const saveBtn = $(".saveBtn");
+const row = $(".row");
 var today = dayjs();
 $("#currentDay").text(today.format("dddd MMM, DD"));
 
@@ -12,7 +13,11 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  saveBtn.on("click", function (event) {
+    let rowId = $(this).closest(".row").attr("id");
+    console.log(rowId);
+  });
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
