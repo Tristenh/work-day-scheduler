49fa45662b5fad07d3cda020215473f347201c1c
Tristenh <tristenhobba1@gmail.com>
2023-06-17 00:59:22 +1000

    added rest of HTML time blocks, added dayjs current day and month

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const container = $(".container-fluid px-5")
+var today = dayjs();
+$("#currentDay").text(today.format("dddd MMM, DD"));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
